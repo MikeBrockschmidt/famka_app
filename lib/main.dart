76a44d1b64bff7163f99a1ed40e4c2a136c2105c
Mike Brockschmidt <info@mike-brockschmidt.de>
2025-07-24 17:38:15 +0200

    noch ein Test

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:famka_app/src/main_app.dart';
 
-//ffsdfdfs
+//ffsdfdfsdfgfgdfgfgd
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
